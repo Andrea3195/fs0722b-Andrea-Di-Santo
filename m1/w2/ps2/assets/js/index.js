9ee65f51db123739eb679a671c48578ae744e1f3
Andrea3195 <andreads95@hotmail.it>
2022-11-04 20:00:48 +0100

    Progetto Settimanale

--- m1/w2/ps2/assets/js/index.js
+++ m1/w2/ps2/assets/js/index.js
@@ -1,2 +1,14 @@
 // Funzioni
 
+function pressButton(dato){ 
+    document.getElementById("display").value += dato.textContent;
+ }
+
+ function clearDisplay() { 
+    document.getElementById("display").value = ""; 
+   }
+ 
+ function equals() { 
+    document.getElementById("display").value = eval(document.getElementById("display").value); 
+ }
+ 
